304cd2b2dec5e6f47f87c91992b0d00f385f34dc
Samstep <SamStep2012@gmail.com>
2017-05-04 21:31:43 -0700

    Command !choose fixed!

--- commands/choose.js
+++ commands/choose.js
@@ -1,8 +1,9 @@
-exports.run = (client, message) => {
+exports.run = (client, message, args) => {
+  let data1 = args[0];
+  let data2 = args[1];
   let chooseVal = Math.floor((Math.random() * 2 ) + 1);
   if (chooseVal == 2) {
-    let data1 = args[0];
-    let data2 = args[1];
+
     message.reply("I choose " + data1);
   } else {
     message.reply("I choose " + data2);
