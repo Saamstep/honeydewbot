cab382940de3dbebe2bb87a72b7304a921443f57
Samstep <SamStep2012@gmail.com>
2017-06-03 23:51:15 -0700

    updated log info and half fixed purge bugs. need to fix args[0] bug

--- commands/@purge.js
+++ commands/@purge.js
@@ -1,33 +1,37 @@
-exports.run = (client, message, args, guild) => {
+exports.run = (client, message, args) => {
   let result = args.join(' ');
   let messagecount = parseInt(result);
+  let guild = message.guild;
   let modRole = message.guild.roles.find("name", "Mods");
-  let logchannel = guild.channels.find("name", "log");
   if (!message.member.roles.has(modRole.id)) {
     return message.reply(":no_entry_sign: | Error. You don't have the right permissions").catch(console.error);
-  } else
-  message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-  message.reply("Bulk deleted messages successfully!");
-  console.log(result);
-
-  logchannel.send('', {embed: {
-    color: 44242,
-    fields: [{
-      name: "Action Log",
-      value: "\u200b"
-    },
-    {
-      name: "Purge",
-      value: `${messagecount}\n\u200b`
-    },
-    {
-      name: "Muted by:",
-      value: `${message.author}`
-    }],
-    timestamp: new Date(),
-    footer: {
-      text: 'Logged by honeydewbot',
+  } else {
+    message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+    message.reply("Bulk deleted messages successfully!");
+    let logchannel = guild.channels.find("name", "log");
+    console.log(message.channel + " BULK DELETED IN");
+    logchannel.send('', {embed: {
+      color: 44242,
+      fields: [{
+        name: "Action Log",
+        value: "\u200b"
+      },
+      {
+        name: "Purged",
+        value: `${result} messages\n\u200b`
+      },
+      {
+        name: "By:",
+        value: `${message.author}\n\u200b`
+      },
+      {
+        name: "Channel:",
+        value: `${message.channel}`
+      }],
+      timestamp: new Date(),
+      footer: {
+        text: 'Logged by honeydewbot',
+      }
     }
-  }
-  });
-};
+    });
+  }};
