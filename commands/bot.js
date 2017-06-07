60535d5f754ac13ae3defe7e720331f60aebc420
Samstep <SamStep2012@gmail.com>
2017-06-06 23:33:49 -0700

    fixed config.version! added it to appropriate files :)

--- commands/bot.js
+++ commands/bot.js
@@ -1,11 +1,16 @@
 exports.run = (client, message, args) => {
+  const config = require("../config.json");
   message.channel.sendEmbed({ color: 3447003,
     title: "Creator:",
     description: "This bot was made by Samstep",
     fields: [{
       name: "Social Links:",
-      value: "[YouTube](http://www.bit.ly/subtosamstep) **|** [Twitch](http://www.twitch.tv/saamstep) **|** [Twitter](http://www.twitter.com/saamstep)"
-
-    }]
+      value: "[YouTube](http://www.bit.ly/subtosamstep) **|** [Twitch](http://www.twitch.tv/saamstep) **|** [Twitter](http://www.twitter.com/saamstep) **|** [GitHub](http://www.github.com/saamstep/honeydewbot)"
+    },
+    {
+      name: "Bot Version:",
+      value: `${config.version}`
+    }
+    ]
   });
 };
